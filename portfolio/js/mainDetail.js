10d41e8b44490ee8e8d49d36690501ef659f65f2
Sahin Cinar <sahicina@student.arteveldehs.be>
2023-02-23 01:27:10 +0100

    f

--- portfolio/js/mainDetail.js
+++ portfolio/js/mainDetail.js
@@ -30,10 +30,12 @@ scene.add(pointLight, ambientLight);
 
 function addStars() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 12);
-  const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff
+  });
   const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3) .fill() .map(() => THREE.MathUtils.randFloatSpread(150));
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(150));
 
   star.position.set(x, y, z);
   scene.add(star);
